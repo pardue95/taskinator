275114ef9ac9d57656e2a8843d7171ca777d2743
pardue95 <cwpardue@gmail.com>
2021-10-09 10:58:28 -0500

    completed array for persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,12 +173,14 @@ var taskStatusChangeHandler = function(event) {
         tasksCompletedEl.appendChild(taskSelected);
     };
 
-    // update task's in tasks array
-    // for (var i = 0; i < tasks.length; i++) {
-    //     if (tasks[i].id === parseInt(taskId)) {
-    //         tasks[i].status = statusValue;
-    //     };
+ // update task's in tasks array
+for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === parseInt(taskId)) {
+      tasks[i].status = statusValue;
+    }
+  };
 
+console.log(tasks);
         saveTasks();
 
     
@@ -214,14 +216,19 @@ var deleteTask = function(taskId) {
     taskSelected.remove();
 
     // create new array to hold updated list of tasks
-    var updatedTaskArr = [];
+// create new array to hold updated list of tasks
+var updatedTaskArr = [];
 
-    // loop through current tasks
-    // for (var i = 0; i < tasks.length; i++) {
-    //     // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
-    //     if (tasks[i].id !== parseInt(taskId)) {
-    //         updatedTaskArr.push(tasks[i]);
-    //     };
+// loop through current tasks
+for (var i = 0; i < tasks.length; i++) {
+  // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+  if (tasks[i].id !== parseInt(taskId)) {
+    updatedTaskArr.push(tasks[i]);
+  }
+}
+
+// reassign tasks array to be the same as updatedTaskArr
+tasks = updatedTaskArr;
 
         saveTasks();
     };
